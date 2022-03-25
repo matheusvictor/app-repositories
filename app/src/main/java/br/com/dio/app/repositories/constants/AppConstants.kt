80c42b32e0378d385ca9c7a4cc7ceb45b770e1d2
Matheus Victor <matheusvictor.salles@gmail.com>
2022-03-25 15:46:09 -0300

    Cria módulo de dados e chamadas de serviço

---
+++ app/src/main/java/br/com/dio/app/repositories/constants/AppConstants.kt
@@ -0,0 +1,13 @@
+package br.com.dio.app.repositories.constants
+
+class AppConstants private constructor() {
+
+    object API_URL {
+        const val BASE_URL = "https://api.github.com/"
+    }
+
+    object HTTP {
+        const val SUCCESS = 200
+    }
+
+}
